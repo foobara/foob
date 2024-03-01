1e9cc9d593d209bf1b85f61b130dd7a02af14504
Miles Georgi <azimux@gmail.com>
2024-03-01 09:35:51 -0800

    1709314551

--- src/commands/help.rb
+++ src/commands/help.rb
@@ -4,14 +4,13 @@ module Foobara
       module Commands
         class Help < ShCliConnector::Commands::Help
           def command_class
-            binding.pry
             return @command_class if defined?(@command_class)
 
             @command_class = if argument
-                               generate_command_class = generator_key_to_command_class(argument)
+                               generate_command_class = command_connector.generator_key_to_command_class(argument)
 
                                if generate_command_class
-                                 transform_command_class(generate_command_class)
+                                 command_connector.transform_command_class(generate_command_class)
                                else
                                  super
                                end
