830605528d8cf2fd75ae4429788d3f1883c48f27
Miles Georgi <azimux@gmail.com>
2024-04-02 23:45:04 +0000

    Make tests pass with new generator arguments

--- spec/foob_spec.rb
+++ spec/foob_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Foobara::CommandConnectors::Foob do
     [
       "g",
       "ruby-project",
-      "-p",
+      "-n",
       project_name,
       "--output_directory",
       output_directory,
@@ -15,7 +15,7 @@ RSpec.describe Foobara::CommandConnectors::Foob do
       "some@email.com"
     ]
   end
-  let(:project_name) { "Some::Test::Project" }
+  let(:project_name) { "some-test-project" }
   let(:output_directory) { "#{__dir__}/../../fake_project_generated_from_test_suite" }
   let(:description) { "some fake project generated from the test suite." }
   let(:foob) { described_class.new(program_name: "foob") }
